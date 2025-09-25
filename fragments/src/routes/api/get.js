5f54744cb5562cb78925449e600003067240aefd
Daniel Park <Dpark39@myseneca.ca>
2025-09-25 11:14:11 -0400

    code refactor

--- fragments/src/routes/api/get.js
+++ fragments/src/routes/api/get.js
@@ -1,13 +1,9 @@
 // src/routes/api/get.js
 
-/**
- * Get a list of fragments for the current user
- */
+const { createSuccessResponse } = require('../../response'); 
+
 module.exports = (req, res) => {
-  // TODO: this is just a placeholder. To get something working, return an empty array...
-  res.status(200).json({
-    status: 'ok',
-    // TODO: change me
-    fragments: [],
-  });
+  // placeholder: return empty array for now
+  res.status(200).json(createSuccessResponse({ fragments: [] }));
+
 };
